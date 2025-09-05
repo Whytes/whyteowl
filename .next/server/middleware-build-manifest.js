b04d79bb9597da8242d5d3f75111c8bace47567d
whytes <gforewilson@gmail.com>
2025-09-05 03:02:34 -0400

    login fix attempt

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,11 +24,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/auth/signin.js"
-    ],
-    "/auth/signup": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/auth/signup.js"
     ]
   },
   "ampFirstPages": []
